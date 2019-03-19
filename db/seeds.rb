fa085c954a55afbaa76b29f8bbe1c02dd640d4c2
KaiKaspar <kai.k.b.shovlin@gmail.com>
2019-03-19 11:07:03 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a1 = Artist.create(name: 'bob')
+a2 = Artist.create(name: 'bill')
+a3 = Artist.create(name: 'bruce')
+
+s1 = Song.create(title: 'tegkyjecgr', artist_id: '1')
+s2 = Song.create(title: 'relkjf', artist_id: '2')
+s3 = Song.create(title: '2erjv', artist_id: '3')
